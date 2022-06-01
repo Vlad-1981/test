ddd621acabc2e7a0d7fe28169c28a59f35105a83
Vlad-1981 <svlad_minsk@mail.ru>
2022-06-01 09:20:57 +0300

    dell func "minus"

--- vsearch4web.py
+++ vsearch4web.py
@@ -126,10 +126,6 @@ def view_the_log() -> 'html':
                            the_data=contents)
 
 
-def minus(a, b):
-    return a - b
-
 if __name__ == "__main__":
     app.run(debug=True)
 
-    print(minus(5, 10))
