b720d8c0cf4c9643fae787691a48532789c40324
Vlad-1981 <svlad_minsk@mail.ru>
2022-06-01 09:19:24 +0300

    add 3 function: "sum", "minus", "mul"

---
+++ list_func.py
@@ -0,0 +1,26 @@
+def plus(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+if __name__ == '__main__':
+
+    print(plus(7, 8))
+    print(minus(17, 3))
+    print(mul(12, 4))
+
+
+
+
+
+
+
+
+
